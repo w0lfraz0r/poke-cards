da3316a490c62f7492fe5ad038184c8747493c47
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2024-06-20 18:36:10 +0530

    css changes in card added

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -20,15 +20,32 @@ function PokemonCard({ url }) {
   }
 
   return (
-    <div className="bg-grey shadow-md rounded-lg overflow-hidden flex flex-col p-4">
-      <img
-        className="w-50 h-48 object-cover mx-auto"
-        src={pokemon.sprites.front_default}
-        alt={pokemon.name}
-      />
-      <h3 className="text-xl font-bold text-center mt-4">{pokemon.name}</h3>
-      <div className="flex flex-wrap justify-center mt-2">
-        {/* Display other pokemon data here later*/}
+    <div className="flex items-center justify-center min-w-50vw min-h-fit mt-7 bg-gray-100">
+      <div className="bg-gray-200 shadow-md rounded-lg overflow-hidden flex flex-col p-4">
+        <img
+          className="w-50 h-48 object-cover mx-auto"
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name}
+        />
+        <h3 className="text-xl font-bold text-center mt-4 capitalize">{pokemon.name}</h3>
+        <div className="flex flex-wrap justify-center mt-2">
+          <div className="text-center m-2">
+            <span className="block font-semibold">Height</span>
+            <span>{pokemon.height / 10} m</span>
+          </div>
+          <div className="text-center m-2">
+            <span className="block font-semibold">Weight</span>
+            <span>{pokemon.weight / 10} kg</span>
+          </div>
+          <div className="text-center m-2">
+            <span className="block font-semibold">Type</span>
+            <span className="capitalize">{pokemon.types.map(type => type.type.name).join(', ')}</span>
+          </div>
+          <div className="text-center m-2">
+            <span className="block font-semibold">Abilities</span>
+            <span className="capitalize">{pokemon.abilities.map(ability => ability.ability.name).join(', ')}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
